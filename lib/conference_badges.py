b46cafe73c47839f5cc1749d0e6a6633957ee92a
Galen Sho Sato <galen.sato@gmail.com>
2023-02-24 15:35:56 -0500

    Completed Lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,29 @@
+
+
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    name_list = []
+    for name in names:
+        badge = badge_maker(name)
+        name_list.append(badge)
+    return name_list
+        
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for i in range (1, 9):
+        message = f"Hello, {names[i-1]}! You'll be assigned to room {i}!"
+        rooms.append(message)
+    return rooms
 
 def printer(names):
-    return None
+    badge_names = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for name in badge_names:
+        print(name)
+    for message in rooms:
+        print(message)
+    
+
